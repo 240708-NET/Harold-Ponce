6d9c321f0adba71e330c0bfdd46676d194712a3d
Harold Ponce <hponceco@yahoo.com>
2024-07-12 04:13:19 -0400

    Completed Project with Comments

--- P0-Bank Simulator/Account.cs
+++ P0-Bank Simulator/Account.cs
@@ -1,27 +1,39 @@
-﻿class Account{
+﻿class Account
+{
+    //Variables to represent Account Information.
     String name = "";
     int accountNumber = 0;
     double balance = 0;
 
-    public Account(String name, int accountNumber,double balance){
+    //Constructor Method.
+    public Account(String name, int accountNumber, double balance)
+    {
         this.name = name;
         this.accountNumber = accountNumber;
         this.balance = balance;
-    }    
+    }
 
-    public int getAccountNumber(){
+    //This method will get the Account Number.
+    public int getAccountNumber()
+    {
         return accountNumber;
     }
 
-    public double getBalance(){
+    //This method will get the Balance Amount.
+    public double getBalance()
+    {
         return balance;
     }
 
-    public void setBalance(double balance){
-        this.balance = balance;       
+    //This method will set the Balance Amount.
+    public void setBalance(double balance)
+    {
+        this.balance = balance;
     }
 
-    public override string ToString(){
-        return "Name: " + name + "\nAccount Number: " + accountNumber + "\nBalance: $" + balance; 
+    //This method will display Account Information.
+    public override string ToString()
+    {
+        return "Name: " + name + "\nAccount Number: " + accountNumber + "\nBalance: $" + balance;
     }
 }
--- P0-Bank Simulator/Program.cs
+++ P0-Bank Simulator/Program.cs
@@ -1,106 +1,166 @@
-class Program{
-
-    public static int menu(){   
+class Program
+{
+    //This method displays the menu options and returns an integer based on the option selected.
+    public static int menu()
+    {
         int option = 0;
-        
+
         Console.WriteLine("1) Withdrawal");
         Console.WriteLine("2) Deposit");
         Console.WriteLine("3) Balance Inquiry");
         Console.WriteLine("4) Exit Account");
+        Console.WriteLine("5) Exit Program");
 
-        while(option < 1 || option > 4){
-            Console.WriteLine("\nPlease select an option(1-4):");
+        //The user must enter a valid option (1-5).
+        while (option < 1 || option > 5)
+        {
+            Console.WriteLine("\nPlease select an option(1-5):");
             option = Int32.Parse(Console.ReadLine());
         }
-                       
-       return option;                
+
+        return option;
     }
 
-    static void Main(String[] args){
+    //This method will determine if a given Account Number already exists.
+    public static bool accountNumberExists(Account[] accounts, int accountNumber)
+    {
+        for (int i = 0; i < accounts.Length; i++)
+        {
+            if (accounts[i] != null && accountNumber == accounts[i].getAccountNumber())
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    //Main method.
+    static void Main(String[] args)
+    {
+        //Ask user how many bank accounts he wants to use in the program.
         Console.WriteLine("How many accounts do you want to create?");
         int numberOfAccounts = Int32.Parse(Console.ReadLine());
-        int index  = 0;
+        int index = 0;
         int option = 0;
         int condition = 0;
 
+        //Create an array to store Account objects.
         Account[] accounts = new Account[numberOfAccounts];
         int count = 1;
 
-        
-        for(int i = 0;i<numberOfAccounts;i++){
-            Console.WriteLine("\nEnter Full Name for Account: " + count);
+        //Ask user to input necessary information for each account.
+        for (int i = 0; i < numberOfAccounts; i++)
+        {
+            //Ask user to enter the Name of the Account Holder.
+            Console.WriteLine("\nEnter Name of the Account Holder for Account: " + count);
             String name = Console.ReadLine();
-
-
+            //Ask user to enter the Account Number.
             Console.WriteLine("Enter Account Number for Account: " + count);
             int accountNumber = Int32.Parse(Console.ReadLine());
+            //Make sure no given Account Number is duplicate.
+            while (accountNumberExists(accounts, accountNumber))
+            {
+                Console.WriteLine("Account Number already exists.");
+                Console.WriteLine("Enter Account Number for Account: " + count);
+                accountNumber = Int32.Parse(Console.ReadLine());
+            }
+            //Ask user to enter the Balance Amount.
             Console.WriteLine("Enter of Balance Amount for Account: " + count);
-            double balance = double.Parse(Console.ReadLine()); 
+            double balance = double.Parse(Console.ReadLine());
             count++;
 
-            accounts[i] = new Account(name,accountNumber,balance);
+            //Create new Account Objects by calling the constructor.
+            accounts[i] = new Account(name, accountNumber, balance);
         }
 
         bool accountExists = false;
-        Console.WriteLine("----------------------------");
-        while(!accountExists){
-        Console.WriteLine("Please enter Account Number:");
-        int accNumber = Int32.Parse(Console.ReadLine()); 
-        for(int i = 0; i<accounts.Length;i++){
-            if(accNumber == accounts[i].getAccountNumber()){
-                index = i;
-                accountExists = true;
-                condition = 1;
+        bool running = true;
+
+        while (running)
+        {
+            Console.WriteLine("----------------------------");
+            //Repeatedly ask the user to enter an Account Number until a valid Account Number is found.
+            while (!accountExists)
+            {
+                Console.WriteLine("Please enter Account Number:");
+                int accNumber = Int32.Parse(Console.ReadLine());
+
+                //Iterate through Accounts array to find Account with matching accoung number.
+                for (int i = 0; i < accounts.Length; i++)
+                {
+                    if (accNumber == accounts[i].getAccountNumber())
+                    {
+                        //Keep track of index of the Account that was found.
+                        index = i;
+
+                        accountExists = true;
+                        condition = 1;
+                    }
+                }
+
+                // Prompt user if account is not found.
+                if (accountExists == false)
+                {
+                    Console.WriteLine("Account does not exist.\n");
+                }
+            }
+
+            while (accountExists)
+            {
+                while (condition == 1)
+                {
+                    Console.WriteLine("----------------------------");
+                    Console.WriteLine("Account Number: " + accounts[index].getAccountNumber());
+                    //Display menu and keep track of the selected option.
+                    option = menu();
+
+                    switch (option)
+                    {
+                        //Option 1 will withdraw from Account.
+                        case 1:
+                            Console.WriteLine("How much cash do you want to withdraw?");
+                            double withdraw = double.Parse(Console.ReadLine());
+                            if (accounts[index].getBalance() >= withdraw)
+                            {
+                                accounts[index].setBalance(accounts[index].getBalance() - withdraw);
+                            }
+                            else
+                            {
+                                Console.WriteLine("Insufficient Balance");
+                            }
+                            break;
+                        //Option 2 will deposit to Account.
+                        case 2:
+                            Console.WriteLine("How much cash do you want to deposit?");
+                            double deposit = double.Parse(Console.ReadLine());
+                            if (deposit < 0)
+                            {
+                                Console.WriteLine("Invalid amount");
+                            }
+                            else
+                            {
+                                accounts[index].setBalance(accounts[index].getBalance() + deposit);
+                            }
+                            break;
+                        //Option 3 will display Balace Inquiry.
+                        case 3:
+                            Console.WriteLine("\n" + accounts[index].ToString() + "\n");
+                            break;
+                        //Option 4 will exit the Account.
+                        case 4:
+                            condition = 0;
+                            break;
+                        //Option 5 will exit the Program.
+                        case 5:
+                            condition = 0;
+                            running = false;
+                            break;
+                        default:
+                            break;
+                    }
+                }
+                accountExists = false;
             }
         }
-
-        if(accountExists == false){
-            Console.WriteLine("Account does not exist.\n");
-        }
     }
-
-    while(accountExists){
-        while(condition == 1){
-            Console.WriteLine("----------------------------");
-        Console.WriteLine("Account Number: " + accounts[index].getAccountNumber());
-        option = menu();
-
-        switch(option){
-            case 1:
-                Console.WriteLine("How much cash do you want to withdraw?");
-                double withdraw = double.Parse(Console.ReadLine());
-                if(accounts[index].getBalance() >= withdraw){
-                    accounts[index].setBalance(accounts[index].getBalance() - withdraw);
-                }
-                else{
-                    Console.WriteLine("Insufficient Balance");
-                }
-                break;
-            case 2:   
-                Console.WriteLine("How much cash do you want to deposit?");
-                double deposit = double.Parse(Console.ReadLine());
-                if(deposit < 0){
-                    Console.WriteLine("Invalid amount");
-                }
-                else{
-                    accounts[index].setBalance(accounts[index].getBalance() + deposit);
-                }     
-                break;
-            case 3:
-                Console.WriteLine("\n" + accounts[index].ToString() + "\n");
-                break;
-            case 4:
-                condition = 0;
-                break;
-            default:
-                break;
-
-        }
-        }
-        
-        accountExists = false;
-    }
-
-    }
-    
 }
